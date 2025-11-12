2a7926c2c32502e3ad3b3e9266b80c5e3942f112
Chai Jengyi <95327700+epicguy149@users.noreply.github.com>
2025-11-12 20:59:40 +1100

    created banner component

---
+++ frontend/src/components/AppBanner.tsx
@@ -0,0 +1,24 @@
+import Banner from '@atlaskit/banner';
+import WarningIcon from '@atlaskit/icon/core/status-warning';
+import ErrorIcon from '@atlaskit/icon/core/status-error';
+import StatusInformationIcon from '@atlaskit/icon/core/status-information';
+import { useAppContext } from '../AppContext';
+
+const icons = {
+    warning: <WarningIcon label="Warning" />,
+    error: <ErrorIcon label="Error" />,
+    announcement: <StatusInformationIcon label="Info" />
+} as const;
+
+export default function AppBanner() {
+    const { banner } = useAppContext();
+    if (!banner) {
+        return null;
+    }
+
+    return (
+        <Banner appearance={banner.type} icon={icons[banner.type]}>
+            {banner.message}
+        </Banner>
+    )
+}
