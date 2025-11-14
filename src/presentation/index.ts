9ca4f222a1824bf0833489978b874ba63e5bc06f
Chai Jengyi <95327700+epicguy149@users.noreply.github.com>
2025-11-14 21:47:52 +1100

    defined getBacklog resolver

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,5 +1,11 @@
 import Resolver from '@forge/resolver';
-
+import { GetBacklogRequestSchema, type GetBacklogResponse } from '../../contracts/api';
+import { getBacklog } from '../application/get-backlog';
 const resolver = new Resolver();
+
+resolver.define('getBacklog', async(req): Promise<GetBacklogResponse> => {
+  const payload = GetBacklogRequestSchema.parse(req.payload);
+  return getBacklog(payload);
+})
 
 export const handler = resolver.getDefinitions();
