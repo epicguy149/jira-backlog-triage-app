86174269c3ecaa8662e34daa5853cbbe1c223b76
Chai Jengyi <95327700+epicguy149@users.noreply.github.com>
2025-11-14 19:15:06 +1100

    added zod schemas for getbacklog api

---
+++ contracts/api/get-backlog.contract.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+
+// GET /rest/agile/1.0/board/{boardId}/backlog RESPONSE
+export const SwipeIssueSchema = z.object({
+    id: z.string(),
+    key: z.string(),
+    summary: z.string(),
+    status: z.string(),
+    priority: z.string().nullable(),
+    asignee: z.string().nullable(),
+
+    // for future use
+    description: z.string().nullable(),
+    // to filter swiped/unswiped
+    swiped: z.boolean().optional().default(false),
+    aiSummary: z.string().nullable().optional(),
+    // updated if issue updated since aiSummary generated
+    aiSummaryStale: z.boolean().optional(),
+})
+
+export type SwipeIssue = z.infer<typeof SwipeIssueSchema>;
+
+// GET /rest/agile/1.0/board/{boardId}/backlog RESPONSE
+export const SwipeIssuePageSchema = z.object({
+    issues: z.array(SwipeIssueSchema),
+    maxResults: z.number().int(),
+    startAt: z.number().int(),
+    total: z.number().int(),
+})
+
+export type SwipeIssuePage = z.infer<typeof SwipeIssuePageSchema>;
+
+// GET /rest/agile/1.0/board/{boardId}/backlog
+export const GetBacklogRequestSchema = z.object({
+    // allow number or string
+    boardId: z.union([z.number(), z.string()]),
+
+    startAt: z.number().int().min(0).optional(),
+
+    // default 50
+    maxResults: z.number().int().min(1).optional(),
+})
+
+export type GetBacklogRequest = z.infer<typeof GetBacklogRequestSchema>;
+
+export type GetBacklogResponse = SwipeIssuePage;
+
+
