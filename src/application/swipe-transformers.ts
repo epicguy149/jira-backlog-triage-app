28bd187ed0151639565f9251ff3d762f160ea326
Chai Jengyi <95327700+epicguy149@users.noreply.github.com>
2025-11-15 11:24:44 +1100

    added handling of receiving description as ADF, added swiped logic for future filter usage

--- src/application/swipe-transformers.ts
+++ src/application/swipe-transformers.ts
@@ -17,8 +17,24 @@ type JiraIssue = {
                 '48x48'?: string;
             };
         };
-        description?: string | null;
+        // returns as ADF 
+        description?: unknown;
+        updated?: string;
+    };
+}
+
+// parse from string/ADF (atlassian document format) object into safe string
+function parseDescription(raw: unknown): string | null {
+    if (typeof raw === 'string') {
+        return raw;
     }
+
+    if (raw && typeof raw === 'object') {
+        // TODO: if description in resposne is ADF add parsing logic
+        return 'requires parsing from ADF to text';
+    }
+
+    return null;
 }
 
 export type JiraBacklogResponse = {
@@ -28,12 +44,16 @@ export type JiraBacklogResponse = {
     total: number;
 }
 
-export function toSwipeIssue(issue: JiraIssue): SwipeIssue {
-    const { id, key, fields } = issue;
+export function toSwipeIssue(
+    issue: JiraIssue,
+    opts: { swiped?: boolean } = {},
+): SwipeIssue {
+    const { fields } = issue;
+    const description = parseDescription(fields.description);
 
     return {
-        id,
-        key,
+        id: issue.id,
+        key: issue.key,
         summary: fields.summary,
         status: fields.status?.name ?? 'None',
         priorityName: fields.priority?.name ?? null,
@@ -43,8 +63,8 @@ export function toSwipeIssue(issue: JiraIssue): SwipeIssue {
             fields.assignee?.avatarUrls?.['24x24'] ?? 
             fields.assignee?.avatarUrls?.['48x48'] ??
             null,
-        description: fields.description ?? null,
-        swiped: false,
-    }
+        description,
+        swiped: opts.swiped ?? false,
+    };
 }
 
